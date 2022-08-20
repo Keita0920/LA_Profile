9addbbdd3c9b01b699458096769992264a385143
Keita0920 <cy20255@shibaura-it.ac.jp>
2022-08-20 16:43:27 +0900

    test

---
+++ Profile/Profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by K I on 2022/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
